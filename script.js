1d31145976c1fefe58cb0514d09a9a4f6e20d26d
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-27 10:38:02 +0300

    script.js

--- script.js
+++ script.js
@@ -65,3 +65,23 @@ window.addEventListener('scroll', () => {
     });
 
 });
+
+
+// STICKY HEADER
+
+const header =
+document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 80){
+
+        header.classList.add('sticky');
+
+    }else{
+
+        header.classList.remove('sticky');
+
+    }
+
+});
